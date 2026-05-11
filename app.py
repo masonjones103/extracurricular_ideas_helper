237e4a4a5a022ed6ca4a81b3a796272e450ffef4
masonjones103 <masonjones103@gmail.com>
2026-05-11 12:41:24 -0700

    v0.1 Working state
    Updated app.py, style.css, index.html.
    Removed hello.py, base.html.
    Project now functioning.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from openai import OpenAI
+import markdown
 
 app = Flask(__name__)
 client = OpenAI(
@@ -8,19 +9,25 @@ client = OpenAI(
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html') #references the HTML file
+
+@app.route('/chat', methods=['POST'])
 def chat():
-    user_message = request.json.get("message")
+    user_input = request.json.get("message")
 
     response = client.responses.create(
         prompt={
             "id": "pmpt_69ea771f557081978a3655abdcc4f9dd0c41a47a53eecaf4",
             "version": "3"
-        }
+        },
+        input=user_input
     )
 
+    # converts to markdown
+    formatted_response = markdown.markdown(response.output_text)
+
     return jsonify({
-        "reply": response.output_text
+        "response": formatted_response
     })
 
 if __name__ == "__main__":
